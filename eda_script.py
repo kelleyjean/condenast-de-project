03b82f997f6e5f2ea9729f19526b3e97c91ac715
Kelley Shann <kelleyshann@gmail.com>
2021-07-06 21:04:58 -0400

    Added basic exploratory analysis of sample data script

---
+++ eda_script.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import csv as csv
+import glob
+
+
+#Script that loops through sample CSV data and writes EDA results to .txt file
+
+path = 'sample_data'
+files = glob.glob(path + "/*.csv")
+
+def eda():
+    print("Writing sample data exploratory analysis to file 'eda_info.txt'...")
+    with open('eda_info.txt', 'w') as f:
+        for filename in files:
+            f.write(filename + "\n")
+            data = pd.read_csv(filename)
+            data.info(verbose = True, buf=f)
+
+eda()
