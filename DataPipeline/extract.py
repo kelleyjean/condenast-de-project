1c52668f4b025d1d473f7063dd90f242113f34ff
Kelley Shann <kelleyshann@gmail.com>
2021-07-12 00:09:29 -0400

    restructured files and programs

--- DataPipeline/extract.py
+++ DataPipeline/extract.py
@@ -3,14 +3,12 @@ import csv
 import glob
 import os
 
-path = os.getcwd()
-csv_files = glob.glob(os.path.join(path, "*.csv"))
+csv_files = glob.glob('**/*.csv', recursive=True)
 
 def dataframes_to_pickle():
   for f in csv_files:
     filename = f.split("\\", 1)[1]
     df = pd.read_csv(f)
     df.to_pickle("./pickle_files/" + filename + '.pkl')
-
 
 dataframes_to_pickle()
