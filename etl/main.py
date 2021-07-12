fdb19eee9e9aa40ad3139bb1a3be10cd877d0af1
kelleyjean <kelleyshann@gmail.com>
2021-07-11 23:17:20 -0400

    Delete main.py
    deleted unecessary file

--- etl/main.py
+++
@@ -1,9 +0,0 @@
-import extract.dataframes_to_pickle
-import transform.transformation_1
-import transform.transformation_2
-
-def main():
-    dataframes_to_pickle()
-    transformation_1()
-    transformation_2()
-
